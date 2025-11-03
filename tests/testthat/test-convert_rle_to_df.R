4f1b51ba90112ea07848aec8c293170f32989099
Shelley Gao <syugao@gmail.com>
2025-11-03 15:29:54 -0800

    update prepared_unreliable_regions

--- tests/testthat/test-convert_rle_to_df.R
+++ tests/testthat/test-convert_rle_to_df.R
@@ -1,35 +1,31 @@
-prepared_unreliable_regions <- function() {
-  load(here::here("tests", "data", "unreliable_regions_1_4_0.rda"))
+prepared_unreliable_regions <- (
+  combined_unreliable_regions %>%
+  dplyr::filter(`1_4_0` == TRUE) %>%
+  dplyr::select(Start, End, `Virus segment`, Categories)
+)
 
-  return(unreliable_regions)
-}
-
-rles <- function() {
-  return(
-    rles <- convert_bam_to_rle(here::here("tests", "files", "reads_1.sorted.bam"))
-  )
-}
+rle_list <- convert_bam_to_rle(here::here("tests", "files", "reads_1.sorted.bam"))
 
 test_that("run_with_defaults", {
   expect_snapshot(
-    convert_rle_to_df(rles())
+    convert_rle_to_df(rle_list)
   )
 })
 
 test_that("run_with_unreliable_regions", {
   expect_snapshot(
     convert_rle_to_df(
-      rles(), unreliable_regions = prepared_unreliable_regions()
+      rle_list, unreliable_regions = prepared_unreliable_regions
     )
   )
 })
 
 test_that("unreliable_regions_has_effect", {
-  unmasked_rles <- convert_rle_to_df(rles())
+  unmasked_rles <- convert_rle_to_df(rle_list)
   
   masked_rles <- convert_rle_to_df(
-    rles(),
-    unreliable_regions = prepared_unreliable_regions(),
+    rle_list,
+    unreliable_regions = prepared_unreliable_regions,
   )
 
   expect_snapshot(
@@ -37,27 +33,6 @@ test_that("unreliable_regions_has_effect", {
       unmasked_rles, masked_rles, 
       max_diffs = Inf,
       x_arg = "disabled", y_arg = "enabled"
-    )
-  )
-})
-
-test_that("run_paired", {
-  rles_single <- convert_bam_to_rle(
-    here::here("tests", "files", "paired_1.sorted.bam"), 
-    paired = FALSE
-  )
-  
-  rles_paired <- convert_bam_to_rle(
-    here::here("tests", "files", "paired_1.sorted.bam"), 
-    paired = TRUE
-  )
-
-  expect_snapshot(
-    waldo::compare(
-      convert_rle_to_df(rles_single, unreliable_regions = prepared_unreliable_regions()),
-      convert_rle_to_df(rles_paired, unreliable_regions = prepared_unreliable_regions()), 
-      max_diffs = Inf,
-      x_arg = "single", y_arg = "paired"
     )
   )
 })
